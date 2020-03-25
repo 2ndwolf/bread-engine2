268b505d230d1f7a6455a4db48d4e5224db5f843
Troy Edwards <troyedwardsjr@gmail.com>
2020-03-25 18:49:20 -0400

    Ready up for WebGL

--- client/engine/world/World.cs
+++ client/engine/world/World.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Blazor.Extensions.Canvas.Canvas2D;
 
 namespace LegendOfWorlds.Engine.World {
@@ -8,15 +9,27 @@ namespace LegendOfWorlds.Engine.World {
     public static int x = 0;
     public static int y = 0;
 
+    public static Dictionary<int, Canvas2DContext> canvases;
+
     // Rendering
     public static Canvas2DContext baseCanvas;
 
     public static void Init(Canvas2DContext _baseCanvas) {
       baseCanvas = _baseCanvas;
+      canvases = new Dictionary<int, Canvas2DContext>();
 
       Console.WriteLine("Initializing world!");
 
+      Task.Run(Update);
       Task.Run(Render);
+    }
+
+    public static async Task Update() {
+      for(;;)
+      {
+        Console.WriteLine("Game Logic!");
+        await Task.Delay(16);
+      }
     }
 
     public static async Task Render() {
@@ -24,11 +37,11 @@ namespace LegendOfWorlds.Engine.World {
       {
         x++;
         y++;
-        await baseCanvas.ClearRectAsync(0, 0,500, 500);
+        await baseCanvas.ClearRectAsync(0, 0, 500, 500);
         await baseCanvas.SetFillStyleAsync("green");
         await baseCanvas.FillRectAsync(x, y, 100, 100);
         Console.WriteLine("Rendering!");
-        await Task.Delay(16);
+        await Task.Delay(4);
       }
     }
   }
