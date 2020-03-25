2f750dc56af330a63721763040c26cbf74a1f16d
Troy Edwards <troyedwardsjr@gmail.com>
2020-03-25 18:03:36 -0400

    Test rendering

--- client/engine/world/World.cs
+++ client/engine/world/World.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Threading.Tasks;
+using Blazor.Extensions.Canvas.Canvas2D;
 
 namespace LegendOfWorlds.Engine.World {
   public class World {
-    public World() {
-      Console.WriteLine("HELLO WORLD!!! AHAHA!");
+    // Temp entity position.
+    public static int x = 0;
+    public static int y = 0;
+
+    // Rendering
+    public static Canvas2DContext baseCanvas;
+
+    public static void Init(Canvas2DContext _baseCanvas) {
+      baseCanvas = _baseCanvas;
+
+      Console.WriteLine("Initializing world!");
+
+      Task.Run(Render);
+    }
+
+    public static async Task Render() {
+      for(;;)
+      {
+        x++;
+        y++;
+        await baseCanvas.ClearRectAsync(0, 0,500, 500);
+        await baseCanvas.SetFillStyleAsync("green");
+        await baseCanvas.FillRectAsync(x, y, 100, 100);
+        Console.WriteLine("Rendering!");
+        await Task.Delay(16);
+      }
     }
   }
 }
+
