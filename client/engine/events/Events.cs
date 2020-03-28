c2a1255d214c5124aed0fa068aecbde645eaa4bb
Troy Edwards <troyedwardsjr@gmail.com>
2020-03-28 12:26:10 -0400

    Fix event emitter

--- client/engine/events/Events.cs
+++ client/engine/events/Events.cs
@@ -4,11 +4,11 @@ using System;
 namespace LegendOfWorlds.Engine {
   public static partial class Events {
     // Util func.
-    public static void AddEvent(Action action) {
+    public static void AddEvent(Action<object> action) {
       World.EE.On(nameof(action), action);
     }
     
-    public static void EmitEvent(Action action, dynamic data) {
+    public static void EmitEvent(Action<object> action, dynamic data) {
       World.EE.Emit(nameof(action), data);
     }
     
