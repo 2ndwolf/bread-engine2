56fbdf26d9db647d5ff608bd59d88b6885a89624
Troy Edwards <troyedwardsjr@gmail.com>
2020-03-31 11:53:00 -0400

    More rendering stuff

--- client/wwwroot/js/api.js
+++ client/wwwroot/js/api.js
@@ -5,7 +5,7 @@
   // Render targets
   const targets = new Map()
   // Textures
-  const images = new Map()
+  const textures = new Map()
 
   window.createTarget = async (id, x, y) => {
     const canvas = document.createElement('canvas')
@@ -14,11 +14,15 @@
 
     const ctx = canvas.getContext('2d')
 
+    // schema
     targets.set(id, {
       canvas,
       ctx,
       x,
-      y
+      y,
+      // TODO: Implement multiple textures per target in the future.
+      // textures: new Map() // textureKey, textureArray? or texture
+      // activeTexture: {key, index}
     })
     
     return id
@@ -26,9 +30,28 @@
 
   window.drawOnTarget = (id) => {
     const target = targets.get(id)
-    const image = images.get(id)
-    target.ctx.drawImage(image, 0, 0)
+    const texture = textures.get(id)
+    target.ctx.drawImage(texture, 0, 0)
   }
+
+  window.setTargetPos = (id, x, y) => {
+    const target = targets.get(id)
+    target.x = x
+    target.y = y
+  }
+
+  /*
+  // TODO: Implement multiple textures per target in the future.
+  // textures: new Map() // textureKey, textureArray? or texture
+  // activeTexture: {key, index}
+  window.addTexToTarget = (id) => {
+    
+  }
+
+  window.removeTexFromTarget = (id) => {
+
+  }
+  */
 
   window.drawAllTargets = async () => {
     rootCtx.clearRect(0, 0, rootCanvas.width, rootCanvas.height)
@@ -38,24 +61,26 @@
   }
 
   window.createImage = async (url, id) => {
-    // Create new image object.
+    // Create new texture object.
     const img = new Image()
     img.id = id
     img.src = url
 
-    // Wait until image loads
+    // Wait until texture loads
     await new Promise((resolve) => {
       img.onload = (ev) => {
         resolve(ev)
       }
     })
 
-    images.set(id, img)
+    textures.set(id, img)
   }
+
   (async () => {
     await window.createImage("https://localhost:5001/assets/doll.png", 'derp')
     await window.createTarget('derp', 0, 0)
     await window.drawOnTarget('derp')
     await window.drawAllTargets()
   })()
+
 })()
