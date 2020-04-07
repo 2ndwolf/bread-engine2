f846039fa6a00b559c80a76f19aad81c33d6a9d8
Tryptamine Games <tryptamiengames@gmail.com>
2020-04-06 22:58:09 -0400

    Test Tiled JSON

--- converter/Program.cs
+++ converter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace converter
 {
@@ -7,6 +9,14 @@ namespace converter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FileStream fs = File.OpenRead("../client/wwwroot/assets/test_map.json");
+            byte[] b = new byte[1024];
+            UTF8Encoding temp = new UTF8Encoding(true);
+
+            while (fs.Read(b,0,b.Length) > 0) 
+            {
+                Console.WriteLine(temp.GetString(b));
+            }
         }
     }
 }
---
+++ shared/cells/TiledParser.cs
@@ -0,0 +1,5 @@
+namespace Shared.Cells {
+  public class TiledParser {
+    
+  }
+}
